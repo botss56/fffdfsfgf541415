5463306af0ea67cfc11234d86b307c4e7e611899
botss56 <42004217+botss56@users.noreply.github.com>
2018-08-02 14:19:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame('Bat Community © .','https://www.twitch.tv/OvErDz1');
+  client.user.setGame('Devil Community © .','https://www.twitch.tv/OvErDz1');
   console.log('---------------');
   console.log(' Bot Is Online')
   console.log('---------------')
