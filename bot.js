cf5f9c3299380c30d0390dba24d8f660c731cee1
botss56 <42004217+botss56@users.noreply.github.com>
2018-08-02 14:17:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ var prefix = "1"
 client.on('message', message => {
   if(!message.channel.guild) return;
 if(message.content.startsWith(prefix + 'bc')) {
-if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+if(!message.channel.guild) return message.channel.send('** هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
 if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
 let BcList = new Discord.RichEmbed()
